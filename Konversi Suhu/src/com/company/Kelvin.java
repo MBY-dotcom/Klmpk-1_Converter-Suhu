223b48eaf5c9c46f2a1aa9d4689e55e96dfd75df
Mokhammad Brian Yusuf <72732482+MBY-dotcom@users.noreply.github.com>
2021-11-16 06:44:59 +0700

    Add files via upload

---
+++ Konversi Suhu/src/com/company/Kelvin.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+public class  Kelvin {
+    Main SA = new Main();
+    double toFahrenheit(){
+        //°F = K × 1,8 − 459,67
+        return (SA.SuhuAwal*1.8-459.67);
+    }
+    double toReamur(){
+        //°Ré = (K − 273,15) × 0,8
+        return ((SA.SuhuAwal-273.15)*0.8);
+    }
+    double toCelcius(){
+        //°C = K − 273,15
+        return (SA.SuhuAwal-273.15);
+    }
+}
